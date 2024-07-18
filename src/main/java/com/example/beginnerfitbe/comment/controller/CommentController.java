56f5e0e9bed5200c3bf16f4945b080a2fddfa794
wlstj <22020306jinseoyeon@gmail.com>
2024-07-19 01:49:06 +0900

    feat: comment 조회 구현

--- src/main/java/com/example/beginnerfitbe/comment/controller/CommentController.java
+++ src/main/java/com/example/beginnerfitbe/comment/controller/CommentController.java
@@ -3,13 +3,11 @@ package com.example.beginnerfitbe.comment.controller;
 import com.example.beginnerfitbe.comment.dto.CommentCreateDto;
 import com.example.beginnerfitbe.comment.service.CommentService;
 import com.example.beginnerfitbe.jwt.util.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,10 +16,37 @@ public class CommentController {
 
     private final CommentService commentService;
     private final JwtUtil jwtUtil;
+
     @PostMapping("/create")
+    @Operation(summary = "댓글 생성 메서드", description = "게시글번호/내용을 입력받아 댓글을 생성합니다.")
     private ResponseEntity<?> create(HttpServletRequest request, @RequestBody CommentCreateDto commentCreateDto){
         Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
         return commentService.create(userId, commentCreateDto);
+    }
+
+    @GetMapping("")
+    @Operation(summary = "댓글 조회 메서드", description = "전체 댓글을 조회합니다.")
+    private ResponseEntity<?> list() {
+        return ResponseEntity.ok(commentService.list());
+    }
+
+    @GetMapping("/{commentId}")
+    @Operation(summary = "댓글 상세 조회 메서드", description = "댓글 상세 정보를 조회합니다.")
+    private ResponseEntity<?> read(@PathVariable Long commentId) {
+        return ResponseEntity.ok(commentService.read(commentId));
+    }
+
+    @GetMapping("/me")
+    @Operation(summary = "사용자 댓글 조회 메소드", description = "사용자가 마이페이지에서 자신이 등록한 댓글을 조회합니다.")
+    private ResponseEntity<?> me(HttpServletRequest request) {
+        Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
+        return ResponseEntity.ok(commentService.me(userId));
+    }
+
+    @GetMapping("/post/{postId}")
+    @Operation(summary = "게시글 댓글 조회 메소드", description = "게시글에 등록한 댓글을 조회합니다.")
+    private ResponseEntity<?> getCommentsByPost(@PathVariable Long postId) {
+        return ResponseEntity.ok(commentService.getCommentsByPost(postId));
     }
 
 }
--- src/main/java/com/example/beginnerfitbe/comment/service/CommentService.java
+++ src/main/java/com/example/beginnerfitbe/comment/service/CommentService.java
@@ -2,9 +2,11 @@ package com.example.beginnerfitbe.comment.service;
 
 import com.example.beginnerfitbe.comment.domain.Comment;
 import com.example.beginnerfitbe.comment.dto.CommentCreateDto;
+import com.example.beginnerfitbe.comment.dto.CommentDto;
 import com.example.beginnerfitbe.comment.repository.CommentRepository;
 import com.example.beginnerfitbe.error.StateResponse;
 import com.example.beginnerfitbe.post.domain.Post;
+import com.example.beginnerfitbe.post.dto.PostDto;
 import com.example.beginnerfitbe.post.repository.PostRepository;
 import com.example.beginnerfitbe.user.domain.User;
 import com.example.beginnerfitbe.user.repository.UserRepository;
@@ -13,6 +15,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -35,6 +39,31 @@ public class CommentService {
 
         commentRepository.save(comment);
         return ResponseEntity.ok(StateResponse.builder().code("SUCCESS").message("댓글을 성공적으로 생성했습니다.").build());
+    }
+
+    public List<CommentDto> list(){
+        return commentRepository.findAll().stream()
+                .map(CommentDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public CommentDto read(Long commentId){
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new IllegalArgumentException("not found comment"));
+        return CommentDto.fromEntity(comment);
+    }
+
+    public List<CommentDto> getCommentsByPost(Long postId){
+        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("not found post"));
+        return commentRepository.findCommentsByPost(post).stream()
+                .map(CommentDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+
+    public List<CommentDto> me (Long userId){
+        User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("not found user"));
+        return commentRepository.findCommentsByUser(user).stream()
+                .map(CommentDto::fromEntity)
+                .collect(Collectors.toList());
     }
 
 }
