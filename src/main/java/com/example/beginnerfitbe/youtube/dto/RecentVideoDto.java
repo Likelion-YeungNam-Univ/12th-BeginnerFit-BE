e547b5b7894e76d1ba3911e0a9c0320ff1dd7bb1
wlstj <22020306jinseoyeon@gmail.com>
2024-07-24 21:11:08 +0900

    fix: RecentVideoDto수정

--- src/main/java/com/example/beginnerfitbe/youtube/dto/RecentVideoDto.java
+++ src/main/java/com/example/beginnerfitbe/youtube/dto/RecentVideoDto.java
@@ -6,17 +6,23 @@ import lombok.Data;
 
 @Data
 public class RecentVideoDto {
+    private Long id;
+    private String videoId;
     private String url;
     private String title;
 
     @Builder
-    public RecentVideoDto(String url, String title) {
+    public RecentVideoDto(Long id, String videoId, String url, String title) {
+        this.id = id;
+        this.videoId = videoId;
         this.url = url;
         this.title = title;
     }
 
     public static RecentVideoDto fromEntity(YoutubeVideo youtubeVideo) {
         return RecentVideoDto.builder()
+                .id(youtubeVideo.getId())
+                .videoId(youtubeVideo.getVideoId())
                 .url(youtubeVideo.getUrl())
                 .title(youtubeVideo.getTitle())
                 .build();
