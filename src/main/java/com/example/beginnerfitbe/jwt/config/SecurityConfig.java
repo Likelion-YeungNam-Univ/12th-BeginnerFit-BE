ef7dcc3d5869941ad6ac16bbecb53cc711e8110a
wlstj <22020306jinseoyeon@gmail.com>
2024-07-24 16:15:36 +0900

    chore: securityconfig 수정

--- src/main/java/com/example/beginnerfitbe/jwt/config/SecurityConfig.java
+++ src/main/java/com/example/beginnerfitbe/jwt/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
+        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Collections.singletonList("*"));
         configuration.setAllowCredentials(true); // 인증 정보를 포함한 요청 허용
