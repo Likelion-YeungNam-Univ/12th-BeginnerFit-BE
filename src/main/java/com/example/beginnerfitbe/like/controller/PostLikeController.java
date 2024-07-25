e735bdbebe4a29525cc9c5b53f76580768f383fe
wlstj <22020306jinseoyeon@gmail.com>
2024-07-25 16:28:01 +0900

    chore: operation추가

--- src/main/java/com/example/beginnerfitbe/like/controller/PostLikeController.java
+++ src/main/java/com/example/beginnerfitbe/like/controller/PostLikeController.java
@@ -2,6 +2,7 @@ package com.example.beginnerfitbe.like.controller;
 
 import com.example.beginnerfitbe.jwt.util.JwtUtil;
 import com.example.beginnerfitbe.like.service.PostLikeService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,12 +16,14 @@ public class PostLikeController {
     private final PostLikeService postLikeService;
 
     @PostMapping("/{postId}")
+    @Operation(summary = "게시글 좋아요 메서드", description = "게시글에 좋아요를 생성합니다.")
     public ResponseEntity<?> addLike(HttpServletRequest request, @PathVariable Long postId){
         Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
         return ResponseEntity.ok(postLikeService.create(userId, postId));
     }
 
     @DeleteMapping("/{postId}")
+    @Operation(summary = "게시글 좋아요 취소 메서드", description = "게시글에 생성한 좋아요를 취소합니다.")
     public ResponseEntity<?> deleteLike(HttpServletRequest request, @PathVariable Long postId){
         Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
         return ResponseEntity.ok(postLikeService.delete(userId, postId));
