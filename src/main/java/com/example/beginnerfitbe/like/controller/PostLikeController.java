bbedc4313daf562b96edeaad86e3e9b6551f6d26
wlstj <22020306jinseoyeon@gmail.com>
2024-07-25 16:36:38 +0900

    fix: endpoint 수정

--- src/main/java/com/example/beginnerfitbe/like/controller/PostLikeController.java
+++ src/main/java/com/example/beginnerfitbe/like/controller/PostLikeController.java
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/likes")
+@RequestMapping("/posts")
 public class PostLikeController {
     private final JwtUtil jwtUtil;
     private final PostLikeService postLikeService;
 
-    @PostMapping("/{postId}")
+    @PostMapping("/{postId}/likes")
     @Operation(summary = "게시글 좋아요 메서드", description = "게시글에 좋아요를 생성합니다.")
     public ResponseEntity<?> addLike(HttpServletRequest request, @PathVariable Long postId){
         Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
         return ResponseEntity.ok(postLikeService.create(userId, postId));
     }
 
-    @DeleteMapping("/{postId}")
+    @DeleteMapping("/{postId}/likes")
     @Operation(summary = "게시글 좋아요 취소 메서드", description = "게시글에 생성한 좋아요를 취소합니다.")
     public ResponseEntity<?> deleteLike(HttpServletRequest request, @PathVariable Long postId){
         Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
