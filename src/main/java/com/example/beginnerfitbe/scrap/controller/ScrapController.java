ffedc6dcb7f9dd69ecddb4433bbdc14f814dc41a
wlstj <22020306jinseoyeon@gmail.com>
2024-07-29 16:22:52 +0900

    feat: 스크랩 조회 메소드 구현

--- src/main/java/com/example/beginnerfitbe/scrap/controller/ScrapController.java
+++ src/main/java/com/example/beginnerfitbe/scrap/controller/ScrapController.java
@@ -7,10 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,4 +23,31 @@ public class ScrapController {
         Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
         return ResponseEntity.ok(scrapService.create(userId, postId));
     }
+
+    @GetMapping("/scraps")
+    @Operation(summary = "스크랩 전체 조회 메소드", description = "전체 스크랩 목록을 조회합니다.")
+    private ResponseEntity<?> list(){
+        return ResponseEntity.ok(scrapService.list());
+    }
+
+    @GetMapping("/scraps/me")
+    @Operation(summary = "내가 스크랩 한 내역 조회 메소드", description = "내가 스크랩한 게시글을 조회합니다.")
+    private ResponseEntity<?> getScrapsByUser(HttpServletRequest request){
+        Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
+        return ResponseEntity.ok(scrapService.getScrapsByUser(userId));
+    }
+
+    @GetMapping("/{postId}/scraps")
+    @Operation(summary = "게시글 별 스크랩 조회 메소드", description = "게시글의 스크랩 내역을 조회합니다.")
+    private ResponseEntity<?> getScrapsByPost(@PathVariable Long postId){
+        return ResponseEntity.ok(scrapService.getScrapsByPost(postId));
+    }
+
+    @GetMapping("/{postId}/scraps/check")
+    @Operation(summary = "스크랩 여부 조회 메소드", description = "게시글을 스크랩 했는 지 조회합니다.")
+    private ResponseEntity<?> checkScrap(HttpServletRequest request, @PathVariable Long postId){
+        Long userId = jwtUtil.getUserId(jwtUtil.resolveToken(request).substring(7));
+        return ResponseEntity.ok(scrapService.checkScrap(userId, postId));
+    }
+
 }
--- src/main/java/com/example/beginnerfitbe/scrap/repository/ScrapRepository.java
+++ src/main/java/com/example/beginnerfitbe/scrap/repository/ScrapRepository.java
@@ -6,9 +6,12 @@ import com.example.beginnerfitbe.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     Optional<Scrap> findByUserAndPost(User user, Post post);
+    List<Scrap> findScrapsByUser(User user);
+    List<Scrap> findScrapsByPost(Post post);
 }
