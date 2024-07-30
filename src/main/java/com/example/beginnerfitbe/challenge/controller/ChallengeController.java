c8bffbf88840a13cd56e2ab7c69809a7d206b164
ugauga19892556@gmail.com <ugauga19892556@gmail.com>
2024-07-30 23:10:18 +0900

    test용 메서드 이름 변경

--- src/main/java/com/example/beginnerfitbe/challenge/controller/ChallengeController.java
+++ src/main/java/com/example/beginnerfitbe/challenge/controller/ChallengeController.java
@@ -3,7 +3,7 @@ package com.example.beginnerfitbe.challenge.controller;
 import com.example.beginnerfitbe.challenge.config.ChallengeScheduler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ public class ChallengeController {
     private ChallengeScheduler challengeScheduler;
 
     //예시를 만들기 위한 TEST용 api
-    @GetMapping("/test")
+    @PostMapping("/test")
     public String testSchedule() {
         challengeScheduler.testSchedule(); // 수동 호출
         return "Scheduled task executed!";
