abb7e113861a231816d46d75691b676b7627ee5c
parkha <ugauga19892556@gmail.com>
2024-07-31 15:20:55 +0900

    챌린지 count 및 ranking 수정

--- src/main/java/com/example/beginnerfitbe/challengeparticipant/repository/ChallengeParticipantRepository.java
+++ src/main/java/com/example/beginnerfitbe/challengeparticipant/repository/ChallengeParticipantRepository.java
@@ -1,7 +1,9 @@
 package com.example.beginnerfitbe.challengeparticipant.repository;
 
 import com.example.beginnerfitbe.challengeparticipant.domain.ChallengeParticipant;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -16,5 +18,10 @@ public interface ChallengeParticipantRepository extends JpaRepository<ChallengeP
     List<ChallengeParticipant> findByUserIdAndIsCompletedTrue(Long userId);
     long countByUserIdAndIsCompletedTrueAndChallengeCompletedDate(Long userId, LocalDate date);
     boolean existsByChallengeCompletedDate(LocalDate date);
+
+    // 친구들의 성공 카운트를 위한 메소드 추가
+    @Query("SELECT COUNT(cp) FROM ChallengeParticipant cp WHERE cp.challenge.challengeId = :challengeId AND cp.user.id IN :userIds AND cp.isCompleted = true")
+    long countByChallengeIdAndUserIdIn(@Param("challengeId") Long challengeId, @Param("userIds") List<Long> userIds);
+
 
 }
