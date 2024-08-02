c1185b9ee9b8d205062badd461bbe52dde72e529
parkha <ugauga19892556@gmail.com>
2024-08-02 16:05:13 +0900

    주석수정

--- src/main/java/com/example/beginnerfitbe/alarm/domain/AlarmType.java
+++ src/main/java/com/example/beginnerfitbe/alarm/domain/AlarmType.java
@@ -1,7 +1,7 @@
 package com.example.beginnerfitbe.alarm.domain;
 
 public enum AlarmType {
-    FRIEND_REQUEST,        // 친구 요청
+    FRIEND_REQUEST,  // 친구 요청
     COMMENT_ALARM,  // 게시글 댓글 알림
     CHALLENGE_REMINDER,    // 오늘의 챌린지 남은 알림
     FRIEND_ACCEPTANCE      // 친구 수락 알림
